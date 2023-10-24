35935907a9395eefb7f350ca04ceea16a2fe9436
Daniela2319 <frankvt04@gmail.com>
2023-10-24 18:11:21 -0300

    Conexão HTML e JavaScript

---
+++ JavaScript-01/JavaScript-HTML/app.js
@@ -0,0 +1,11 @@
+function conexao(num1, num2) {
+  if (num1 > num2) {
+    for (let i = 0; i < 3; i++) {
+      console.log("Conexão com um arquivo JavaSript!");
+    }
+  } else {
+    console.log("A conexão não está Ok!");
+  }
+}
+
+conexao(5, 2);
